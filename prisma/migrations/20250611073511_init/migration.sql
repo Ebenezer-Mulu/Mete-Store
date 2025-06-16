4cd12313658970078688dd6d9e7f1902062f5509
Ebenezer Mulu <ebenmulu@gmail.com>
2025-06-16 18:38:36 +0300

    updated

---
+++ prisma/migrations/20250611073511_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT,
+    "categoryId" INTEGER NOT NULL,
+    "image" TEXT[],
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,11 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Seed categories first
+
   await prisma.category.createMany({
     data: [
       { id: 1, name: "Sunglasses" },
       { id: 2, name: "Watches" },
+      { id: 3, name: "Earings" },
+      { id: 4, name: "Rings" },
     ],
     skipDuplicates: true,
   });
@@ -22,7 +24,7 @@ async function main() {
         price: 2600,
         categoryId: 2,
         image: [
-          " https://i.ibb.co/VptWN0fp/1.jpg",
+          "https://i.ibb.co/VptWN0fp/1.jpg",
           "https://i.ibb.co/4nCGxK4L/2.jpg",
           "https://i.ibb.co/pvC1w2cy/3.jpg",
           "https://i.ibb.co/YT4typcV/4.jpg",
@@ -50,7 +52,7 @@ async function main() {
         description:
           "1pc Fashionable Unisex Plano Lenses Square Metal Eyeglasses Frame With High-End Classic Retro Style Eyebrow Details Clear Glasses Accessories",
         price: 1800,
-        categoryId: 2,
+        categoryId: 1,
         image: [
           "https://i.ibb.co/CsMcGmR0/1.jpg",
           "https://i.ibb.co/QvFcKNmb/2.jpg",
@@ -82,7 +84,7 @@ async function main() {
         price: 3400,
         categoryId: 2,
         image: [
-          " https://i.ibb.co/8nfXDDQD/photo-2025-04-15-10-43-25.jpg",
+          "https://i.ibb.co/8nfXDDQD/photo-2025-04-15-10-43-25.jpg",
           "https://i.ibb.co/0jRGgYPJ/photo-2025-04-15-10-43-39.jpg",
           "https://i.ibb.co/FqstKz4b/photo-2025-04-15-10-43-45.jpg",
           "https://i.ibb.co/Zz7NfqHP/photo-2025-04-15-10-43-51.jpg",
@@ -189,22 +191,122 @@ async function main() {
         price: 2100,
         categoryId: 1,
         image: [
-          "  https://i.ibb.co/VW69KDmk/1.jpg",
+          "https://i.ibb.co/VW69KDmk/1.jpg",
           "https://i.ibb.co/chPX6Mpt/2.jpg",
         ],
       },
       {
-        name: " Oval Decorative Glasses",
-        slug: " Oval-Decorative-Glasses",
+        name: "Oval Decorative Glasses",
+        slug: "Oval-Decorative-Glasses",
         description:
           "Vintage Oval Decorative Glasses For Women Small Metal Frame Fashion Glasses Ladies Decorative Glasses Beach Accessories For Women Glasses Shades For Summer Beach Vacation,Outdoor,Travel",
         price: 1700,
         categoryId: 1,
         image: [
-          " https://i.ibb.co/4wXFtwVH/1.jpg",
+          "https://i.ibb.co/4wXFtwVH/1.jpg",
           "https://i.ibb.co/dvNWb8q/2.jpg",
           "https://i.ibb.co/bgcj8LSH/3.jpg",
           "https://i.ibb.co/mr4svB6j/4.jpg",
+        ],
+      },
+      {
+        name: "Geometric Photochromic Glasses",
+        slug: "Geometric-Photochromic-Glasses",
+        description:
+          "1pc Men Geometric Fashion Photochromic Glasses For School Life Daily Clothing Accessories Casual Shades Accessories Beach Accessories Glasses Shades Looks Street Style And Suit For Sweater Jacket Sweatshirt Hoodie Leather Pants And Cargo Pants",
+        price: 2100,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/q3Y2t5sq/1.jpg",
+          "https://i.ibb.co/gnzHgsf/2.jpg",
+          "https://i.ibb.co/wNMDwDrj/3.jpg",
+          "https://i.ibb.co/mk9ypCd/4.jpg",
+          "https://i.ibb.co/3mFjCt8w/5.jpg",
+        ],
+      },
+      {
+        name: "Classic Retro Half Frame",
+        slug: "Retro-Half-Frame",
+        description:
+          "1pc Classic Retro Half Frame High-Definition Student Eyeglasses Clear Glasses Accessories",
+        price: 1800,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/RpxHc86n/1.jpg",
+          "https://i.ibb.co/Z6DQFyNz/2.jpg",
+          "https://i.ibb.co/DDd46z8K/3.jpg",
+          "https://i.ibb.co/LXJY9T4w/4.jpg",
+          "https://i.ibb.co/Fk9GkGnm/5.jpg",
+        ],
+      },
+      {
+        name: "Unisex Plano Lenses",
+        slug: "Unisex-Plano-Lenses",
+        description:
+          "1pc Fashionable Unisex Plano Lenses Square Metal Eyeglasses Frame With High-End Classic Retro Style Eyebrow Details Clear Glasses Accessories",
+        price: 1800,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/CsMcGmR0/1.jpg",
+          "https://i.ibb.co/QvFcKNmb/2.jpg",
+          "https://i.ibb.co/SwN0gFzY/3.jpg",
+          "https://i.ibb.co/LzxQWH28/4.jpg",
+          "https://i.ibb.co/7x1NTg2T/5.jpg",
+          "https://i.ibb.co/pvn5YCYN/6.jpg",
+        ],
+      },
+      {
+        name: "KINGSEVEN Men's Glasses ",
+        slug: "kingseven-new-brand-design-mens-glasses",
+        description:
+          "KINGSEVEN New Brand Design Men's Glasses Polarized Sunglasses  Integrated Lens Fashion Eyewear Oculos de sol",
+        price: 2600,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/4R9GPgDN/1.jpg",
+          "https://i.ibb.co/CKGn7M4K/2.jpg",
+          "https://i.ibb.co/3y9DRdSX/4.jpg",
+        ],
+      },
+      {
+        name: "Polarized Fashionable Eyeglasses ",
+        slug: "Polarized-Fashionable-Eyeglasses",
+        description:
+          "1pc Polarized Fashionable Eyeglasses Frame For Driving, Fishing, Outdoor Activities",
+        price: 1800,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/RTgHNjY6/1.jpg",
+          "https://i.ibb.co/9mHZBJYK/2.jpg",
+          "https://i.ibb.co/Kx1Q3qv3/3.jpg",
+        ],
+      },
+      {
+        name: "Retro Rimless Glasses ",
+        slug: "Retro-Rimless-Glasses",
+        description:
+          "Retro Rimless Glasses for Men Steampunk Sunglasses for Women Sun Glasses Female Fashion Eyeglasses Vintage UV400 Shades Goggles",
+        price: 1800,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/ymrwSz0C/1.jpg",
+          "https://i.ibb.co/7JtnCsM6/2.jpg",
+          "https://i.ibb.co/YBcfKXnY/3.jpg",
+        ],
+      },
+
+      {
+        name: "Round Hip Hop ",
+        slug: "Round-Hip-Hop",
+        description:
+          "Retro Metal Geometric Round Hip Hop Party Decor Fashion Eyeglasses Set Glasses Shades",
+        price: 1600,
+        categoryId: 1,
+        image: [
+          "https://i.ibb.co/nsM1q1ff/1.jpg",
+          "https://i.ibb.co/LDtHrJty/2.jpg",
+          "https://i.ibb.co/wFZdpLDk/3.jpg",
+          "https://i.ibb.co/yFqyJz7j/4.jpg",
         ],
       },
     ],
